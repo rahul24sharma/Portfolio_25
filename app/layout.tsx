38f668b5b2a0159beae8e575db088a46cb72235c
rahul24sharma <sharma.rahul1@northeastern.edu>
2025-05-20 16:48:46 -0400

    few components created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,14 @@
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//   title: "Rahul's Portfolio",
-//   description: "A showcase of Rahul's projects, skills, and experience in web development and design. Explore work samples, technical expertise, and professional achievements.",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-import { ThemeProvider } from "./provider.tsx";
+import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Rahul's Portfolio",
-  description: "A showcase of Rahul's projects, skills, and experience in web development and design. Explore work samples, technical expertise, and professional achievements.",
+  title: "Adrian's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
 };
 
 export default function RootLayout({
