f6a671a4c013025dfdd783d84ca10c5be9987a86
rahul24sharma <sharma.rahul1@northeastern.edu>
2025-05-20 16:53:37 -0400

    few components created

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
+import dynamic from "next/dynamic";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
@@ -12,6 +12,8 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
+  const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
